951845b859f8ac61490c1c53ab53501bc8041032
yuval zentler <yzentler@yahoo.com>
2013-12-29 18:18:55 +0200

    performance

--- hcomp.sh
+++ hcomp.sh
@@ -16,41 +16,40 @@ if [ -f ~/.hadoop_tree_structure ] &&  \
 then
         HADOOP_TREE=~/.hadoop_tree_structure.bck
 fi
-                # check that the cache exists , is not still updated (not changed in last 2 seconds) and not too out of date (not more then 30 minutes old)
+                # check that the cache exists , is not still updated (not changed in last 2 seconds) and not too out of date (not more then 24 hours old)
         if [ -f ${HADOOP_TREE} ] &&  \
         (( $((`stat -c %Y ${HADOOP_TREE}` + 2)) < $((`date +%s` )) )) && \
-        (( $((`stat -c %Y ${HADOOP_TREE}` + 1800)) > $((`date +%s` )) ))
+        (( $((`stat -c %Y ${HADOOP_TREE}` + 86400)) > $((`date +%s` )) ))
 
 then
         ## get completion info from the cache
-        local names1=$(for x in `grep "^${DIR}" ${HADOOP_TREE} | sed "s#\(${DIR}[^/]*/\).*#\1#" | uniq | grep -v "^${DIR}$"`;do echo $x;done)
-        COMPREPLY=( $(compgen -W "${names1}" -- ${cur})  )
+		local names1=$(for x in `grep   "^${DIR}" ${HADOOP_TREE} | sed "s#\(${DIR}[^/]*/\).*#\1#" | uniq | grep -v "^${DIR}$"`;do echo $x;done)
+       
+		COMPREPLY=( $(compgen -W "${names1}" -- ${cur})  )
 
-        # recompute the cache ( in bg)only if 600 secs passed
+        # recompute the cache ( in bg)only if 600 secs passed the create a bckup
         if        [ -f ~/.hadoop_tree_structure ] &&  \
         (( $((`stat -c %Y ~/.hadoop_tree_structure` + 600)) < $((`date +%s` )) ))
         then
-                (hadoop fs -lsr / | awk '{print $8"completetmpahretzzkk"$1}' | sed 's/completetmpahretzzkkd.*$/\//' | sed 's/completetmpahretzzkk-.*$//' | sed 's/completetmpahretzzkkFound//' > ~/.hadoop_tree_structure &)
+                
+				(hadoop fs -lsr / | awk '{print $8"completetmpahretzzkk"$1}' | sed 's/completetmpahretzzkkd.*$/\//' | sed 's/completetmpahretzzkk-.*$//' | sed 's/completetmpahretzzkkFound//' > ~/.hadoop_tree_structure &  cp -f ~/.hadoop_tree_structure ~/.hadoop_tree_structure.bck &)
         fi
 
         #create a backup if there is none
         if        [ ! -f ~/.hadoop_tree_structure.bck ]
         then
-                (cp -f ~/.hadoop_tree_structure ~/.hadoop_tree_structure.bck &)
+                
+				(cp -f ~/.hadoop_tree_structure ~/.hadoop_tree_structure.bck &)
         fi
 
-        # create a backup if 2 seconds pass
-        if        [ -f ~/.hadoop_tree_structure.bck ] &&  \
-        (( $((`stat -c %Y ~/.hadoop_tree_structure.bck` + 2)) < $((`date +%s` )) ))
-        then
-                (cp -f ~/.hadoop_tree_structure ~/.hadoop_tree_structure.bck &)
-        fi
+  
 else
     # get completion info straight from  hdfs
+	echo "getting data from hdfs"
         local names1=$(for x in `hadoop fs -ls ${DIR} | awk '{print $8"completetmpahretzzkk"$1}' | sed 's/completetmpahretzzkkd.*$/\//' | sed 's/completetmpahretzzkk-.*$//' | sed 's/completetmpahretzzkkFound//'`;do echo $x;done)
         COMPREPLY=( $(compgen -W "${names1}" -- ${cur})  )
         # create cache(in bg)
-        (hadoop fs -lsr / | awk '{print $8"completetmpahretzzkk"$1}' | sed 's/completetmpahretzzkkd.*$/\//' | sed 's/completetmpahretzzkk-.*$//' | sed 's/completetmpahretzzkkFound//' > ~/.hadoop_tree_structure &)
+        (hadoop fs -lsr / | awk '{print $8"completetmpahretzzkk"$1}' | sed 's/completetmpahretzzkkd.*$/\//' | sed 's/completetmpahretzzkk-.*$//' | sed 's/completetmpahretzzkkFound//' > ~/.hadoop_tree_structure & cp -f ~/.hadoop_tree_structure ~/.hadoop_tree_structure.bck &)
 
 fi
 
@@ -65,6 +64,9 @@ complete  -o filenames -o nospace -F _hcomp hrmr
 complete  -o filenames -o nospace -F _hcomp hcp
 complete  -o filenames -o nospace -F _hcomp hmv
 
+# compute hash for first time
+
+(hadoop fs -lsr / | awk '{print $8"completetmpahretzzkk"$1}' | sed 's/completetmpahretzzkkd.*$/\//' | sed 's/completetmpahretzzkk-.*$//' | sed 's/completetmpahretzzkkFound//' > ~/.hadoop_tree_structure & cp -f ~/.hadoop_tree_structure ~/.hadoop_tree_structure.bck &)
 
  alias hls='hadoop fs -ls'
  alias hcat='hadoop fs -cat'
